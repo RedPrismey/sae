d729b674e0cffeff446edf86bd174dd4cd1ccee3
Prismey <prismey@murena.io>
2026-01-23 10:06:34 +0100

    finalized hash

--- src/main/java/org/insa/sae/Login_Servlet.java
+++ src/main/java/org/insa/sae/Login_Servlet.java
@@ -1,5 +1,8 @@
 package org.insa.sae;
+
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
 
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
@@ -8,54 +11,49 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 
-
 @WebServlet("/Login_Servlet")
 public class Login_Servlet extends HttpServlet {
 
-    @Override
-protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-    String u = request.getParameter("username");
-    String p = request.getParameter("mdp");
+  @Override
+  protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
     try {
-        
-        User user = User.findByUsername(u);
 
-       
-        if (user != null && user.getPassword().equals(p)) {
-            
-            
-            HttpSession session = request.getSession();
-            session.setAttribute("user", user.getUsername());
-            
-            
-            String roleStr = user.getRole().name(); 
-            
-           
-            String roleSession = switch(user.getRole()) {
-                case admin -> "Admin";
-                case student -> "Etudiant";
-                case teacher -> "Prof";
-            };
-            session.setAttribute("role", roleSession);
+      MessageDigest digest = MessageDigest.getInstance("SHA-256");
 
-           
-            switch (user.getRole()) {
-                case admin -> response.sendRedirect("admin"); // Vers AdminDashboardServlet
-                case student -> response.sendRedirect("etudiant"); // Vers EtudiantServlet
-                case teacher -> response.sendRedirect("prof"); // Vers ProfServlet
-            }
+      String u = request.getParameter("username");
+      String p = new String(digest.digest(request.getParameter("mdp").getBytes(StandardCharsets.UTF_8)));
 
-        } else {
-           
-            request.setAttribute("errorMessage", "Identifiant ou mot de passe incorrect.");
-            request.getRequestDispatcher("login.jsp").forward(request, response);
+      User user = User.findByUsername(u);
+
+      if (user != null && user.getPassword().equals(p)) {
+
+        HttpSession session = request.getSession();
+        session.setAttribute("user", user.getUsername());
+
+        String roleSession = switch (user.getRole()) {
+          case admin -> "Admin";
+          case student -> "Etudiant";
+          case teacher -> "Prof";
+        };
+        session.setAttribute("role", roleSession);
+
+        switch (user.getRole()) {
+          case admin -> response.sendRedirect("admin"); // Vers AdminDashboardServlet
+          case student -> response.sendRedirect("etudiant"); // Vers EtudiantServlet
+          case teacher -> response.sendRedirect("prof"); // Vers ProfServlet
         }
 
-    } catch (Exception e) {
-        e.printStackTrace(); 
-        request.setAttribute("errorMessage", "Erreur technique lors de la connexion.");
+      } else {
+
+        request.setAttribute("errorMessage", "Identifiant ou mot de passe incorrect.");
         request.getRequestDispatcher("login.jsp").forward(request, response);
+      }
+
+    } catch (Exception e) {
+      e.printStackTrace();
+      request.setAttribute("errorMessage", "Erreur technique lors de la connexion.");
+      request.getRequestDispatcher("login.jsp").forward(request, response);
     }
-}
+  }
 }
--- src/main/java/org/insa/sae/SignUp_Servlet.java
+++ src/main/java/org/insa/sae/SignUp_Servlet.java
@@ -1,6 +1,8 @@
 package org.insa.sae;
 
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
 
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
@@ -10,38 +12,35 @@ import jakarta.servlet.http.HttpServletResponse;
 
 @WebServlet("/SignUp_Servlet")
 public class SignUp_Servlet extends HttpServlet {
-    @Override
-protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+  @Override
+  protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
     request.setCharacterEncoding("UTF-8");
-    
+
     try {
-        MessageDigest digest = MessageDigest.getInstance("SHA-256");
+      MessageDigest digest = MessageDigest.getInstance("SHA-256");
 
-        String nom = request.getParameter("nom");
-        String prenom = request.getParameter("prenom");
-        String roleStr = request.getParameter("role");
-        String username = request.getParameter("username");
-        String mdp = digest.digest(request.getParameter("mdp").getBytes(StandardCharsets.UTF_8));;
+      String nom = request.getParameter("nom");
+      String prenom = request.getParameter("prenom");
+      String roleStr = request.getParameter("role");
+      String username = request.getParameter("username");
+      String mdp = new String(digest.digest(request.getParameter("mdp").getBytes(StandardCharsets.UTF_8)));
 
-        
-        Role role = switch(roleStr) {
-            case "Etudiant" -> Role.student;
-            case "Enseignant" -> Role.teacher;
-            case "Admin" -> Role.admin;
-            default -> Role.student;
-        };
+      Role role = switch (roleStr) {
+        case "Etudiant" -> Role.student;
+        case "Enseignant" -> Role.teacher;
+        case "Admin" -> Role.admin;
+        default -> Role.student;
+      };
 
-        
-        User user = new User(0, username, mdp, role, nom, prenom);
-        user.save();
+      User user = new User(0, username, mdp, role, nom, prenom);
+      user.save();
 
-      
-        response.sendRedirect("login.jsp?success=1");
+      response.sendRedirect("login.jsp?success=1");
 
     } catch (Exception e) {
-        e.printStackTrace();
-        request.setAttribute("errorMessage", "Erreur : " + e.getMessage());
-        request.getRequestDispatcher("sign_up.jsp").forward(request, response);
+      e.printStackTrace();
+      request.setAttribute("errorMessage", "Erreur : " + e.getMessage());
+      request.getRequestDispatcher("sign_up.jsp").forward(request, response);
     }
-}
+  }
 }
